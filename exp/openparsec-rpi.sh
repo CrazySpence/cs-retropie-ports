e343c91206cb05278884923fb685973c8d2f2a9a
CrazySpence <phil@philtopia.com>
2017-10-29 15:41:00 +0000

    Initial Commit

---
+++ exp/openparsec-rpi.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+rp_module_id="openparsec-rpi"
+rp_module_desc="OpenParsec - There is no safe distance"
+rp_module_licence="GPL2 https://raw.githubusercontent.com/OpenParsec/openparsec/master/LICENSE"
+rp_module_section="exp"
+rp_module_flags="!mali !osmc !xbian !x86"
+
+function depends_openparsec-rpi() {
+	getDepends libsdl2-mixer-2.0-0 libsdl2-2.0-0 libraspberrypi0
+}
+
+function install_bin_openparsec-rpi() {
+    gitPullOrClone "$md_inst" https://github.com/CrazySpence/openparsec-rpi.git
+}
+
+function configure_openparsec-rpi() {
+    cp /opt/retropie/ports/openparsec-rpi/es/OpenParsec.sh /home/pi/RetroPie/roms/ports/
+}
---
+++ install_experimental.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp exp/*sh /home/pi/RetroPie-Setup/scriptmodules/ports
+
